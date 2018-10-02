ac453e850a23e819af6f3b0f44fb64ca362304ce
IvSergey <imas.spb@mail.ru>
2018-10-02 11:58:32 +0300

    new arr commit

--- weather/src/App.js
+++ weather/src/App.js
@@ -7,41 +7,50 @@ import "bootstrap/dist/css/bootstrap.css";
 
 // import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 
-const PLACES = [
-  { name: "Palo Alto"},
-  { name: "San Jose"},
-  { name: "Santa Cruz"},
-  { name: "Honolulu"}
-];
-console.log(PLACES)
-const city = document.querySelector ("#city");
+// const PLACES = [
+//   { name: "Palo Alto"},
+//   { name: "San Jose"},
+//   { name: "Santa Cruz"},
+//   { name: "Honolulu"}
+// ];
+// console.log(PLACES)
+
 
 class App extends Component {
   constructor() {
     super();
     this.state={
-      activePlace:0
+      activePlace:0,
+      city:[]
     };
   }
+
+  handlerAddCity = () => {
+
+    let name = document.getElementById('city_name');
+    console.log(name.value);
+    this.setState({
+        city: [
+            ...this.state.city,
+            { name: name.value}
+        ]
+    });
+}
+
   render() {
     const activePlace = this.state.activePlace;
     return (
       <div className="App">
-        <input type="text" id="city"></input>
-          <button onClick={()=>{
-            PLACES.push(city.value)
-          }}>Добавить</button>
-        {PLACES.map((place, index) => (
+        <input type="text" id="city_name" placeholder="City name"></input>
+          
+        {this.state.city.map((place, index) => (
           <button key={index} onClick={()=>{
             this.setState({activePlace: index});
           }}>
             {place.name}
           </button>
           ))}
-          <WeatherDisplay 
-            key={activePlace}
-            name={PLACES[activePlace].name}
-          />
+          
       </div>
     );
   }
